5a0ad05c4a652a62776c3bf9ba30c599f2c0f3d0
AZ Huang <aitjcize@gmail.com>
2013-06-07 02:09:35 +0800

    Fix encode error when dealing with unicode filename.

--- copyfuse.py
+++ copyfuse.py
@@ -87,7 +87,7 @@ class CopyAPI:
         # build tree
         self.tree_children[path] = {}
         for child in response['children']:
-            name = str(os.path.basename(child['path']))
+            name = os.path.basename(child['path'])
             ctime = int(child['created_time'])
             if child['modified_time'] == None:
                 mtime = ctime
@@ -228,7 +228,7 @@ class CopyFUSE(LoggingMixIn, Operations):
             st = dict(st_mode=(S_IFDIR | 0755), st_nlink=2)
             st['st_ctime'] = st['st_atime'] = st['st_mtime'] = time.time()
         else:
-            name = str(os.path.basename(path))
+            name = os.path.basename(path)
             objects = self.copy_api.list_objects(os.path.dirname(path))
 
             if name not in objects:
